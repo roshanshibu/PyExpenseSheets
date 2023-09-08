da504f6701cdb684cc4edde12a0a85d442c5b0ac
Roshan Shibu <roshanshibu97@gmail.com>
2023-09-08 14:49:42 +0000

    added test call

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ updateLocalDB()
 # get_month_expenses
 # - month - optional (current month when empty), numeric value (eg: August = 8)
 # - categories - optional (no filter by caregory applied when empty), a list of categories to filter by
-# print(*db.get_month_expenses(month=8, categories=["Groceries", "Home"]), sep="\n")
-# print(*db.get_month_expenses(), sep="\n")  # all expenses of current month
+# print(*db.get_month_expenses(month=4, categories=["Groceries", "Home"]), sep="\n")
+# print(*db.get_month_expenses(month=4), sep="\n")  # all expenses from April
+# print(*db.get_month_expenses(), sep="\n") # all expenses of current month
 
 # print(db.get_sum_of_expenses(month=3, categories=["Groceries"]))
